cc45550c6e8b98be25ed50daed4d7b305d838955
jumper32126 <jumper32126@gmail.com>
2016-05-19 15:42:15 +0900

    phpプログラミング基礎学習２　課題２

---
+++ php2.2.php
@@ -0,0 +1,13 @@
+<?php
+$num = 'A';
+$messege = ' ';
+switch($num){
+	case 'A':
+		$messege ='「英語」';
+		break;
+        case 'あ';
+                $messege ='「日本語」';
+                break;
+    }
+    echo $messege;
+ ?>
