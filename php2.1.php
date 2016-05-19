d9fdefbcdbc5749ba7b79d30cb4376edd7d6f285
jumper32126 <jumper32126@gmail.com>
2016-05-19 15:15:13 +0900

    PHPプログラミング基礎学習２　課題１

---
+++ php2.1.php
@@ -0,0 +1,16 @@
+<?php
+$num =' ';
+$messege = ' ' ;
+switch($num){
+	case 1:
+		$messege = '「one」';
+		break;
+        case 2:
+        	$messege ='「two」';
+        	break;
+       default:
+       	        $messege ='「想定外」';
+       	        break;
+   }
+   echo $messege;
+   ?>
