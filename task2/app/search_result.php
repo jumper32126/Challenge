6f85d46be5b72dc67c907a26955d359d2662474a
jumper32126 <jumper32126@gmail.com>
2016-07-25 16:52:41 +0900

    修正しました

---
+++ task2/app/search_result.php
@@ -0,0 +1,50 @@
+<?php
+require_once '../common/defineUtil.php';
+require_once '../common/scriptUtil.php';
+require_once '../common/dbaccesUtil.php';
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+      <title>検索結果画面</title>
+</head>
+    <body>
+      <h1>検索結果</h1>
+        <table border=1>
+            <tr>
+                <th>名前</th>
+                <th>生年</th>
+                <th>種別</th>
+                <th>登録日時</th>
+            </tr>
+        <?php
+        //$result = null;
+        //var_dump($_GET['name']);
+        //var_dump($_GET['year']);
+        //var_dump($_GET['type']);
+        if(!isset($_GET['type'])){ //追加修正 種別の存在照明
+           $_GET['type'] = null;
+        }
+        if(empty($_GET['name']) && empty($_GET['year']) && empty($_GET['type'])){
+            $result = search_all_profiles(); //スペル修正aを追加
+        }else{
+            $result = search_profiles($_GET['name'],$_GET['year'],$_GET['type']); //スペル修正aを追加
+        }
+        foreach($result as $value){
+        ?>
+            <tr>
+                <td><a href="<?php echo RESULT_DETAIL ?>?id=<?php echo $value['userID']?>"><?php echo $value['name']; ?></a></td>
+                <td><?php echo $value['birthday']; ?></td>
+                <td><?php echo ex_typenum($value['type']); ?></td>
+                <td><?php echo date('Y年n月j日G時i分s秒', strtotime($value['newDate']));?></td>  <?php // 修正;が一つ多い?>
+            </tr>
+        <?php
+      // }
+        }?>
+      </table>
+    <?php echo return_top();?>
+
+
+</body>
+</html>
---
+++ task2/app/update_result.php
@@ -0,0 +1,54 @@
+<?php
+require_once '../common/defineUtil.php';
+require_once '../common/scriptUtil.php';
+require_once '../common/dbaccesUtil.php';
+ session_start();
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+      <title>更新結果画面</title>
+</head>
+  <body><?php
+    //$_POST['mode'] = isset($_POST['mode'])?$_POST['mode'] :'';  // 追加修正undefined回避
+    if(!isset ($_POST['mode']) || !$_POST['mode']=="RESULT"){ //追加修正 直接アクセス禁止
+        echo 'アクセスルートが不正です。もう一度トップページからやり直してください<br>';
+    }else{?>
+
+  <? $id = $_POST['id'];
+     $name = $_POST['name'];
+     $birthday[0] = $_POST['year'];
+     $birthday[1] = $_POST['month'];
+     $birthday[2] = $_POST['day'];
+     $tell = $_POST['tell'];
+     $type = $_POST['type'];
+     $comment = $_POST['comment'];
+  ?>
+    <?php
+    $birthday = array($birthday[0],$birthday[1],$birthday[2]); //追加修正 関数の引数で使う為にexplodeでバラしたものをまとめる
+    $birthday = implode("-",$birthday);
+
+    $result = update_profile($name,$birthday,$tell,$type,$comment,$id);//追加修正 引数を追加
+    //エラーが発生しなければ表示を行う
+    if(!isset($result)){
+    ?>
+    <h1>更新確認</h1>
+    以上の内容で更新しました。<br>
+
+    名前:<?php echo $name.'<br>';?>     <? // 追加修正 変更データを表示?>
+    誕生日:<?php echo $birthday.'<br>';?>
+    電話番号:<?php echo $tell.'<br>';?>
+    種別:<?php echo ex_typenum($type).'<br>';?>
+    自己紹介文:<?php echo $comment.'<br>';?>
+
+    <?php
+    }else{
+        echo 'データの更新に失敗しました。次記のエラーにより処理を中断します:'.$result;
+    }
+    }
+        echo return_top();
+    ?>
+  </body>
+</html>
